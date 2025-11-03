033d5257bb330d944942c64cb3012214190d4fb5
moazashraf-ma <240026487+moazashraf-ma@users.noreply.github.com>
2025-11-03 05:33:29 +0500

    Update app.py
    changed port for render

--- certifact-backend/app.py
+++ certifact-backend/app.py
@@ -475,4 +475,8 @@ def download_report(result_id):
 
 # ================== RUN SERVER ==================
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
